22f4b3a1d65d197aa3c1132e3392b8836be6d957
anunde <a.nunez.torre@gmail.com>
2024-12-20 12:09:09 +0100

    fix: rollback last changes

--- src/Cart/Application/AddProductToCart/AddProductToCartCommandHandler.php
+++ src/Cart/Application/AddProductToCart/AddProductToCartCommandHandler.php
@@ -6,7 +6,6 @@ use App\Cart\Domain\Entity\Cart;
 use App\Cart\Domain\Entity\CartItem;
 use App\Cart\Domain\Repository\ICartRepository;
 use App\Shared\Domain\Entity\CustomerId;
-use App\Shared\Domain\VO\Sku;
 
 final readonly class AddProductToCartCommandHandler
 {
@@ -16,22 +15,20 @@ final readonly class AddProductToCartCommandHandler
 
     public function __invoke(AddProductToCartCommand $command): void 
     {
+        $cartItem = CartItem::create(
+            $command->getType(),
+            $command->getColor(),
+            $command->getSize(),
+            $command->getPrice(),
+            $command->getCurrency(),
+            $command->getQuantity()
+        );
+
         if(null === $cart = $this->repository->findByCustomerId(new CustomerId($command->getCustomerId()))) {
             $cart = Cart::create($command->getCustomerId());
         }
 
-        if(null === $item = $cart->findItem(new Sku($command->getType(), $command->getColor(), $command->getSize()))) {
-            $item = CartItem::create(
-                $command->getType(),
-                $command->getColor(),
-                $command->getSize(),
-                $command->getPrice(),
-                $command->getCurrency(),
-                $command->getQuantity()
-            );
-        }
-
-        $cart->addCartItem($item);
+        $cart->addCartItem($cartItem);
         $this->repository->save($cart);
     }
 }
