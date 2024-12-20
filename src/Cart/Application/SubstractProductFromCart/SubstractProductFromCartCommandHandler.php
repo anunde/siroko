34150dcb34cf998312fd87ab8543345bf7dd11e5
anunde <a.nunez.torre@gmail.com>
2024-12-20 12:26:40 +0100

    fix: do not allow to modify cart after being in process_payment

--- src/Cart/Application/SubstractProductFromCart/SubstractProductFromCartCommandHandler.php
+++ src/Cart/Application/SubstractProductFromCart/SubstractProductFromCartCommandHandler.php
@@ -2,7 +2,9 @@
 
 namespace App\Cart\Application\SubstractProductFromCart;
 
+use App\Cart\Domain\Exception\CartInProcessPaymentException;
 use App\Cart\Domain\Repository\ICartRepository;
+use App\Cart\Domain\ValueObject\CartStatus;
 use App\Shared\Domain\Entity\CustomerId;
 use App\Shared\Domain\Exception\NotFoundException;
 
@@ -18,6 +20,10 @@ final readonly class SubstractProductFromCartCommandHandler
 
         if (null === $cart) {
             throw new NotFoundException("You do not have any cart created!");
+        }
+
+        if ($cart->getCartStatus()->isEqualTo(CartStatus::processPayment())) {
+            throw new CartInProcessPaymentException("Cart is in process of payment and cannot be modified");
         }
 
         $item = $cart->findItem($command->getSku());
